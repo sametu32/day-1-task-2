792626489649db242def43c5db9c42ce4bf106e9
Yavuz Yayla <y.s.yayla@hotmail.com>
2016-12-28 17:59:58 +0300

    Task 4 OK

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,35 +6,33 @@ using System.Threading.Tasks;
 using System.Net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-
+using Newtonsoft.Json.Linq;
 
 namespace ConsoleApplication1
 {
     class Program
     {
-        private void JSONSerilaize()
-        {
-            
-        }
+
         static void Main(string[] args)
         {
-            // Serializaion  
-            Lesson ders = new Lesson();
-            ders.Name = "Manas";
-            ders.Code = "India";
-
-            // Convert Employee object to JOSN string format   
-            string jsonData = JsonConvert.SerializeObject(ders);
-
-            Console.Write(jsonData);
+            var json = new WebClient().DownloadString("https://raw.githubusercontent.com/mhs/world-currencies/master/currencies.json");
+            Console.Write(json);
+            var Doviz_listesi = JsonConvert.DeserializeObject<Bilgi[]>(json);
+            for(int i=0; i<Doviz_listesi.Length; i++)
+            {
+            Console.WriteLine(Doviz_listesi[i].name);
+            Console.Write(Doviz_listesi[i].symbol);
+            Console.Write(Doviz_listesi[i].cc);
+            }
             Console.Read();
         }
     }
-    public class Lesson {
+    public class Bilgi {
 
-        public string Name { get; set; }
-        public string Code { get; set; }
-
+        
+        public string cc { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
     }
  
 }
