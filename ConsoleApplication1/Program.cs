c31a0a9a8892073b80c42ddaec0a3383882ae691
Yavuz Yayla <y.s.yayla@hotmail.com>
2016-12-28 16:08:33 +0300

    jsonserialize

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
 
 namespace ConsoleApplication1
 {
     class Program
     {
+        private void JSONSerilaize()
+        {
+            
+        }
         static void Main(string[] args)
         {
-            Console.Write("HELLOWORLD");
+            // Serializaion  
+            Lesson ders = new Lesson();
+            ders.Name = "Manas";
+            ders.Code = "India";
+
+            // Convert Employee object to JOSN string format   
+            string jsonData = JsonConvert.SerializeObject(ders);
+
+            Console.Write(jsonData);
             Console.Read();
         }
     }
+    public class Lesson {
+
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+    }
+ 
 }
